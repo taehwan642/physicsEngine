6b2303f7857343c1fabb3932c828dbe8f8f74c39
taehwan642 <taehwan642@naver.com>
2023-03-28 22:22:23 +0900

    Updated cmake to use glfw, updated test code

--- source/test.cpp
+++ source/test.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <Vector.h>
+#include <glfw/glfw3.h>
+
 int main()
 {
-    Math::Vector v = Math::Vector();
-    v.x = 10;
-    std::cout << "Hello World " << v.GetX();
+    if (!glfwInit()) return -1;
+    std::cout << "GLFW lib Init" << std::endl; 
     return 0;
 }
